7255253639d9ed940f8c2b29021ec595e0dd4939
ئشائخعی4030 <m.mahmoudreza1@yahoo.com>
2017-11-04 23:17:18 +0330

    test

--- app/src/main/java/ir/mahmoud/app/MainActivity.java
+++ app/src/main/java/ir/mahmoud/app/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(MainActivity.this, "aaaa",Toast.LENGTH_SHORT).show();
+        String a = "sssss";
     }
 }
