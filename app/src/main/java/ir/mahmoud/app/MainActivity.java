a035ebe96643929338fa875dd0583d925ef213d3
ئشائخعی4030 <m.mahmoudreza1@yahoo.com>
2017-11-04 20:09:45 +0330

    first Comit

--- app/src/main/java/ir/mahmoud/app/MainActivity.java
+++ app/src/main/java/ir/mahmoud/app/MainActivity.java
@@ -2,7 +2,6 @@ package ir.mahmoud.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
