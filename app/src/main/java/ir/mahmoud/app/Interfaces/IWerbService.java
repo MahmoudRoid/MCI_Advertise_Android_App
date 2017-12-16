59b9d0103c8036122f5ef9fdd0e722fe63c8356b
ئشائخعی4030 <m.mahmoudreza1@yahoo.com>
2017-12-16 17:56:00 +0330

    IWebService

---
+++ app/src/main/java/ir/mahmoud/app/Interfaces/IWerbService.java
@@ -0,0 +1,9 @@
+package ir.mahmoud.app.Interfaces;
+
+
+
+public interface IWerbService {
+
+    void getResult(Object result) throws Exception;
+    void getError(String ErrorCodeTitle)throws Exception;
+}
