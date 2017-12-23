8770211e4c0d59d8e82934459be8f0fd549a3bae
KingStar <amir.it67@yahoo.com>
2017-12-23 23:29:15 +0330

    second commit

---
+++ app/src/main/java/ir/mahmoud/app/Adapters/ListAdapter.java
@@ -0,0 +1,110 @@
+package ir.mahmoud.app.Adapters;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import ir.mahmoud.app.Activities.VideoDetailActivity;
+import ir.mahmoud.app.Holders.PostHolder;
+import ir.mahmoud.app.Models.PostModel;
+import ir.mahmoud.app.R;
+import retrofit2.Call;
+import retrofit2.Callback;
+
+
+public class ListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private final int VIEW_TYPE_ITEM = 1;
+    //private final int VIEW_TYPE_LOADING = 2;
+    private List<PostModel> feedItemList;
+    private ProgressBar pb;
+    private int s = 0;
+    private Context mContext;
+
+    public ListAdapter(Context context, List<PostModel> feedItemList, ProgressBar pb) {
+        this.feedItemList = feedItemList;
+        this.mContext = context;
+        this.pb = pb;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, final int i) {
+
+        if (i == VIEW_TYPE_ITEM) {
+            View v;
+            v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_list, null);
+            return new PostHolder(v);
+        }/* else if (i == VIEW_TYPE_LOADING) {
+            View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_loading, null);
+            return new LoadingViewHolder(view);
+        }*/
+        return null;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        /*if (feedItemList.get(position) == null)
+            return VIEW_TYPE_LOADING;
+        else*/
+            return VIEW_TYPE_ITEM;
+    }
+
+    public void onBindViewHolder(final RecyclerView.ViewHolder holder, final int i) {
+
+        if (holder instanceof PostHolder) {
+
+            final PostHolder Holder;
+            try {
+                Holder = (PostHolder) holder;
+                Holder.setIsRecyclable(false);
+                Holder.txt_title.setText(feedItemList.get(i).getTitle());
+                Holder.txt_date.setText(feedItemList.get(i).getDate());
+                Glide.with(mContext).load(feedItemList.get(i).getImageUrl())
+                        .apply(new RequestOptions().placeholder(R.mipmap.homeb)
+                                .diskCacheStrategy(DiskCacheStrategy.RESOURCE))
+                        .into(((PostHolder) holder).img_post);
+            } catch (Exception e) {
+            }
+        }
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent;
+                intent = new Intent(mContext,VideoDetailActivity.class);
+                intent.putExtra("feedItem",  feedItemList.get(i));
+                mContext.startActivity(intent);
+            }
+
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return (null != feedItemList ? feedItemList.size() : 0);
+    }
+
+    public void addItem(PostModel post) {
+        this.feedItemList.add(post);
+        notifyDataSetChanged();
+    }
+}
---
+++ app/src/main/java/ir/mahmoud/app/Asynktask/getListPostsAsynkTask.java
@@ -0,0 +1,92 @@
+package ir.mahmoud.app.Asynktask;
+
+
+import android.content.Context;
+import android.widget.LinearLayout;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ir.mahmoud.app.Classes.Application;
+import ir.mahmoud.app.Interfaces.ApiClient;
+import ir.mahmoud.app.Interfaces.ApiInterface;
+import ir.mahmoud.app.Interfaces.IWebService2;
+import ir.mahmoud.app.Interfaces.IWerbService;
+import ir.mahmoud.app.Models.PostModel;
+import ir.mahmoud.app.Models.SlideShowModel;
+import ir.mahmoud.app.R;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+
+public class getListPostsAsynkTask {
+
+    public Context cn;
+    private IWebService2 delegate = null;
+    private String slug;
+    private List<PostModel> feed = new ArrayList<>() ;
+
+    public getListPostsAsynkTask(final Context cn,final List<PostModel> feed , final IWebService2 m, final  String slug)
+    {
+        this.cn = cn;
+        this.feed = feed;
+        this.delegate = m;
+        this.slug = slug;
+    }
+
+    public void getListPosts() {
+        Call<ResponseBody> call =
+                ApiClient.getClient().create(ApiInterface.class).getPosts(slug);
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, retrofit2.Response<ResponseBody> response) {
+
+                try {
+                    JSONObject obj = new JSONObject(response.body().string());
+                    JSONArray jary = new JSONArray(obj.getString(cn.getString(R.string.posts)));
+                    for (int i = 0; i< jary.length() ; i++)
+                    {
+                        PostModel item = new PostModel();
+                        item.setId(jary.getJSONObject(i).getInt(cn.getString(R.string.id)));
+                        item.setTitle(jary.getJSONObject(i).getString(cn.getString(R.string.title)));
+                        item.setContent(jary.getJSONObject(i).getString(cn.getString(R.string.excerpt)));
+                        item.setDate(jary.getJSONObject(i).getString(cn.getString(R.string.date)));
+
+                        JSONArray jary2 = new JSONArray(jary.getJSONObject(i).getString(cn.getString(R.string.categories)));
+                        item.setCategoryTitle(jary2.getJSONObject(0).getString(cn.getString(R.string.title)));
+
+                        try {
+                            JSONArray jary3 = new JSONArray(jary.getJSONObject(i).getString(cn.getString(R.string.tags)));
+                            item.setTagSlug(jary3.getJSONObject(0).getString(cn.getString(R.string.slug)));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        ///item.setTagSlug();
+
+                        try {
+                            jary2 = new JSONArray(jary.getJSONObject(i).getString(cn.getString(R.string.attachments)));
+                            item.setVideoUrl(jary2.getJSONObject(0).getString(cn.getString(R.string.url)));
+
+                            item.setImageUrl(jary.getJSONObject(i).getJSONObject(cn.getString(R.string.thumbnail_images)).getJSONObject(cn.getString(R.string.thumbnail)).getString(cn.getString(R.string.url)));
+                        } catch (Exception e) {
+                        }
+                         feed.add(item);
+                    }
+                    delegate.getResult(feed);
+                } catch (Exception e) {
+
+                }
+            }
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+
+            }
+        });
+    }
+}
+
+
