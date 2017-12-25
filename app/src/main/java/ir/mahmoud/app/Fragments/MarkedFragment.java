00a5c7f8edb7304a06a0818169f485b77ee56aa9
ZAMAN <amir.it67@yahoo.com>
2017-12-25 15:02:08 +0330

    first commit

--- app/src/main/java/ir/mahmoud/app/Fragments/MarkedFragment.java
+++ app/src/main/java/ir/mahmoud/app/Fragments/MarkedFragment.java
@@ -1,6 +1,7 @@
 package ir.mahmoud.app.Fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -9,24 +10,30 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.orm.query.Select;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import ir.mahmoud.app.Adapters.ListAdapter;
-import ir.mahmoud.app.Interfaces.IWebService2;
+import ir.mahmoud.app.Activities.VideoDetailActivity;
+import ir.mahmoud.app.Adapters.MarkedAdapter;
+import ir.mahmoud.app.Classes.RecyclerItemClickListener;
 import ir.mahmoud.app.Models.PostModel;
+import ir.mahmoud.app.Models.tbl_PostModel;
 import ir.mahmoud.app.R;
 
 public class MarkedFragment extends Fragment {
 
+    List<tbl_PostModel> list;
     @BindView(R.id.pb)
     ProgressBar pb;
     @BindView(R.id.rv)
     RecyclerView rv;
-    ListAdapter adapter;
+    MarkedAdapter adapter;
     private List<PostModel> feed = new ArrayList<>();
     View rootView = null;
 
@@ -38,15 +45,44 @@ public class MarkedFragment extends Fragment {
         ButterKnife.bind(getActivity());
         rv = rootView.findViewById(R.id.rv);
         pb = rootView.findViewById(R.id.pb);
-        setAdapter();
+
+        list = Select.from(tbl_PostModel.class).list();
+        if(list.size()>0){
+            // show list
+            showList(list);
+        }
+        else Toast.makeText(getActivity(), "no record", Toast.LENGTH_SHORT).show();
 
         return rootView;
     }
 
-    private void setAdapter() {
-        LinearLayoutManager lm = new LinearLayoutManager(getActivity());
+    private void showList(final List<tbl_PostModel> list) {
+        LinearLayoutManager lm = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
         rv.setLayoutManager(lm);
-        adapter = new ListAdapter(getActivity(), feed, pb);
+        adapter = new MarkedAdapter(getActivity(), list);
         rv.setAdapter(adapter);
+
+        rv.addOnItemTouchListener(new RecyclerItemClickListener(getActivity(), new RecyclerItemClickListener.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Intent intent = new Intent(getActivity(), VideoDetailActivity.class);
+                intent.putExtra("feedItem",convertToModel(list.get(position)));
+                startActivity(intent);
+            }
+        }));
     }
+
+    public PostModel convertToModel(tbl_PostModel tbl){
+        PostModel  model = new PostModel();
+        model.id= tbl.getPostid();
+        model.title= tbl.getTitle();
+        model.content= tbl.getContent();
+        model.date= tbl.getDate();
+        model.categoryTitle= tbl.getCategorytitle();
+        model.videoUrl= tbl.getVideourl();
+        model.imageUrl= tbl.getImageurl();
+        model.tagSlug= tbl.getTagslug();
+        return model;
+    }
+
 }
